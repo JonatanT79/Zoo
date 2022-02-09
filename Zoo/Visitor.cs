6fef4d285ac8797d5f1601aac7039c28fde25a92
Jonatan Tran <jonathan.t79@hotmail.com>
2022-02-09 18:58:20 +0100

    Added new visitor class

---
+++ Zoo/Visitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    class Visitor
+    {
+        public TimeSpan ArriveTime { get; set; }
+        public TimeSpan LeaveTime { get; set; }
+        public DateTime VisitingDate { get; set; }
+    }
+}
